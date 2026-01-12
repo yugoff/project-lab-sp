ff6319e2a7a193895047e1f47ec7d2421f2c4e74
v.yugov <v.yugov@air-burg.ru>
2026-01-12 15:57:30 +0500

    feat/vector-db

--- backend/endpoints/chat.py
+++ backend/endpoints/chat.py
@@ -1,20 +1,38 @@
 from fastapi import APIRouter
 from schemas.schema_chat import ChatRequest, ChatResponse
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import faiss
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
 
 
 router = APIRouter()
-
-model_name = "Qwen/Qwen2.5-3B-Instruct"
+dataset = load_dataset("wikipedia", "20220301.en", split="train[:1000]")
+documents = [item["text"] for item in dataset]
+# model_name = "Qwen/Qwen2.5-3B-Instruct"
+embed_model = SentenceTransformer("all-MiniLM-L6-v2")
+document_embeddings = embed_model.encode(documents, normalize_embeddings=True)
+model_name = "Qwen/Qwen3-0.6B"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
 
+dimension = document_embeddings.shape[1]
+index = faiss.IndexFlatIP(dimension)
+index.add(document_embeddings)
+
+def get_context(query: str, k: int = 5):
+    query_vector = embed_model.encode([query], normalize_embeddings=True)
+    D, I = index.search(query_vector, k)
+    return "\n".join([documents[i] for i in I[0]])
+
+
 def generate_answer(query: str, mode: str, language: str, max_length: int = 256):
-    prompt = f"Ты {mode} ассистент. Отвечай на вопрос на языке {language}:\n{query}"
+    prompt = f"Ты {mode} ассистент. Отвечай на вопрос на языке {language}:\n{get_context(query)}"
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
     output = model.generate(**inputs, max_new_tokens=max_length)
     answer = tokenizer.decode(output[0], skip_special_tokens=True)
     return answer
+
 
 @router.post("/chat", response_model=ChatResponse)
 async def chat_endpoint(request: ChatRequest):
--- frontend/app.py
+++ frontend/app.py
@@ -7,15 +7,24 @@ st.title("RAG Ассистент")
 if "history" not in st.session_state:
     st.session_state.history = []
 
+if st.button("Доступ к сервису?"):
+    try:
+        r = requests.get("http://127.0.0.1:8000/health")
+        if r.status_code == 200:
+            st.success(r.json())
+        else:
+            st.error(f"Ошибка сервиса: {r.status_code}")
+    except Exception as e:
+        st.error(f"Ошибка запроса: {e}")
+
 operator = st.selectbox("Выберите роль ассистента", ["ERP", "HR", "Finance"])
 language = st.selectbox("Язык", ["ru", "en"])
-
 query = st.text_input("Ваш вопрос:")
 
 if st.button("Отправить") and query:
     payload = {"query": query, "mode": operator, "language": language}
     try:
-        r = requests.post("http://0.0.0.0:8000/chat", json=payload)
+        r = requests.post("http://127.0.0.1:8000/chat", json=payload)
         data = r.json()
         st.session_state.history.append(("Вы", query))
         st.session_state.history.append(("Бот", data["answer"]))
